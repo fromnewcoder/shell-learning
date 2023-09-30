7ce71c8e8f249240700219dfc3073ba14eac64ee
jim <jim@ggsd>
2023-09-30 20:07:59 +0800

    schedule job with at or crontab -e, argument.sh

---
+++ argument.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "You enteed the argument: $1"
+
---
+++ scheduling.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+logfile=/home/jim/shell-learning/job_results.log
+
+/usr/bin/echo "The script ran at the following : $(/usr/bin/date)" >> $logfile
